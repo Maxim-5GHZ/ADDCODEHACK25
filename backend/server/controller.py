a955becae1b59866852602bc16bccfc084e020aa
Poker <studentmail0@yandex.ru>
2025-10-05 18:00:08 +0300

    Updated CORS middleware allowed address

--- backend/server/controller.py
+++ backend/server/controller.py
@@ -37,7 +37,7 @@ class controller():
 
         self.app.add_middleware(
             CORSMiddleware,
-            allow_origins=["http://localhost:3000"],  # URL вашего React приложения
+            allow_origins=["http://localhost:5137"],
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
