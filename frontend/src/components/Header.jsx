f62fff25c6212b4478f1718847e7413ce7313dd5
Poker <studentmail0@yandex.ru>
2025-10-07 22:55:13 +0300

    Added profile navigation in the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,31 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import logoSVG from "../assets/logo.svg"
+// import { getCookie } from "../utils/cookies";
 
 function Header({ isTransparent }) {
+    const [user, setUser] = useState(null);
     let navButtonsStyle = isTransparent ? "text-[var(--neutral-color)] hover:text-[var(--accent-light-color)]" : "text-[var(--neutral-dark-color)] hover:text-[var(--accent-dark-color)]"
+
+    useEffect(() => {
+        const checkAuth = () => {
+            // const token = getCookie('token');
+            const token = true; // Временная заглушка
+            
+            if (token) {
+                // Временная заглушка
+                setUser({
+                    name: "Иван",
+                    surname: "Иванов"
+                });
+            } else {
+                setUser(null);
+            }
+        };
+
+        checkAuth();
+    }, []);
+
     return (
     <header id='header' className={`shadow-md fixed w-dvw h-1/14 z-20 ${isTransparent ? "bg-[var(--header-blur)] backdrop-blur-xs" : "bg-[var(--neutral-color)]" }`}>
         <div className="px-32 h-full">
@@ -30,12 +53,20 @@ function Header({ isTransparent }) {
                 </nav>
 
                 <div className="flex items-center gap-x-16">
-                    <Link to="/login" className={`${navButtonsStyle} text-3xl transition-colors duration-100"`}>
-                        Войти
-                    </Link>
-                    <Link to="/registration" className={`${navButtonsStyle} text-3xl transition-colors duration-100"`}>
-                        Зарегистрироваться
-                    </Link>
+                    {user ? (
+                        <Link to="/profile" className={`${navButtonsStyle} text-3xl`}>
+                            {user.name} {user.surname}
+                        </Link>
+                    ) : (
+                        <>
+                            <Link to="/login" className={`${navButtonsStyle} text-3xl transition-colors duration-100"`}>
+                                Войти
+                            </Link>
+                            <Link to="/registration" className={`${navButtonsStyle} text-3xl transition-colors duration-100"`}>
+                                Зарегистрироваться
+                            </Link>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
