ce124b91788ebc0e9f448e88509f5b06a2c64043
Poker <studentmail0@yandex.ru>
2025-10-17 23:20:21 +0300

    Revert "Remove profile buttons"
    This reverts commit aa755443701d2a4e8fa2bd311b9a7baf19ec45d2.

--- frontend/src/components/Profile/ProfileTab.jsx
+++ frontend/src/components/Profile/ProfileTab.jsx
@@ -10,8 +10,6 @@ function ProfileTab({ user }) {
         <div className="text-2xl font-semibold text-[var(--neutral-dark-color)]">{user.email}</div>
       </div>
 
-      {/* Кнопки для редактирования профиля и смены пароля (на сервере нет эндпоинта, поэтому кнопки убраны) */}
-      {/* 
       <div className="flex gap-6">
         <button
           className="bg-[var(--accent-color)] hover:bg-[var(--accent-light-color)]
@@ -24,7 +22,6 @@ function ProfileTab({ user }) {
           Сменить пароль
         </button>
       </div>
-      */}
     </>
   );
 }
